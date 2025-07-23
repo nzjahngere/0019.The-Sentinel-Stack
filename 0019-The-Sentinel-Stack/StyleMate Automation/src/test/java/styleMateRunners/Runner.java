ec79f791842ea58703ac7b79322487a8f71a2c20
Nazish Jehangir <ahtesham72@gmail.com>
2025-07-23 09:35:52 +0530

    Add files via upload

--- 0019-The-Sentinel-Stack/StyleMate Automation/src/test/java/styleMateRunners/Runner.java
+++ 0019-The-Sentinel-Stack/StyleMate Automation/src/test/java/styleMateRunners/Runner.java
@@ -5,7 +5,12 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		features = {"E:\\\\STS\\\\workspace-spring-tools-for-eclipse-4.31.0.RELEASE\\\\TheSentinelStack\\\\StyleMate Features\\\\Homepage.feature",
-				    "E:\\\\\\\\STS\\\\\\\\workspace-spring-tools-for-eclipse-4.31.0.RELEASE\\\\\\\\TheSentinelStack\\\\\\\\StyleMate Features\\\\\\\\Catalog.feature"},
+				    "E:\\\\\\\\STS\\\\\\\\workspace-spring-tools-for-eclipse-4.31.0.RELEASE\\\\\\\\TheSentinelStack\\\\\\\\StyleMate Features\\\\\\\\Catalog.feature",
+				    "E:\\\\\\\\STS\\\\\\\\workspace-spring-tools-for-eclipse-4.31.0.RELEASE\\\\\\\\TheSentinelStack\\\\\\\\StyleMate Features\\\\\\\\CartAndWishlist.feature",
+				    "E:\\\\\\\\STS\\\\\\\\workspace-spring-tools-for-eclipse-4.31.0.RELEASE\\\\\\\\TheSentinelStack\\\\\\\\StyleMate Features\\\\\\\\Checkout.feature",
+				    "E:\\\\STS\\\\workspace-spring-tools-for-eclipse-4.31.0.RELEASE\\\\TheSentinelStack\\\\StyleMate Features\\\\OrderManagement.feature",
+				    "E:\\\\STS\\\\workspace-spring-tools-for-eclipse-4.31.0.RELEASE\\\\TheSentinelStack\\\\StyleMate Features\\\\UserManagement.feature"},
+		
 		glue = {"styleMateAutomation"}
 		)
 
